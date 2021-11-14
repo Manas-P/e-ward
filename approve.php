1a6668c9b2d6aed89c95bc2b95d3aa7cd964e1c1
Manas P <manas4518pachakkil@gmail.com>
2021-11-15 02:02:40 +0530

    Mailing Details Added

--- approve.php
+++ approve.php
@@ -8,15 +8,34 @@
     //     echo $row['email'];
     // }
     
-    //Generate Random Password
+    // Generate Random Password
     $length=8;
     $generatedPassword='';
     $validChar='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
     while(0<$length--){
         $generatedPassword.=$validChar[random_int(0,strlen($validChar)-1)];
     }
+    //============================Send Mail==================================
+    //Get user mail
+    $userMail="SELECT * FROM `tbl_registration` WHERE `rid`='$id'";
+    $mailResult=mysqli_query($conn,$userMail);
+    while ($row = mysqli_fetch_assoc($mailResult))
+    {
+        $toMail= $row['email'];
+        $houseNo=$row['houseno'];
+        $name=$row['fname'];
+    }
+    
+    //Mail Informations
+    $subject="E-Ward Approved";
+    $body="Dear $name, your request have approved. You can login to E-Ward using Id = $houseNo and Password = $generatedPassword";
+    $headers="From: ewardmember@gmail.com";
 
-    //Update User Status
-    $updateQuery="UPDATE `tbl_registration` SET `password`='$generatedPassword',`status`=1 WHERE `rid`='$id'";
-    $updateResult=mysqli_query($conn,$updateQuery);
+    if(mail($toMail,$subject,$body,$headers)){
+        //Update User Status and Password
+        $updateQuery="UPDATE `tbl_registration` SET `password`='$generatedPassword',`status`=1 WHERE `rid`='$id'";
+        $updateResult=mysqli_query($conn,$updateQuery);
+    }else{
+        echo "Mail not Send";
+    }
 ?>
