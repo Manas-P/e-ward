6178923de3d5302515846c15e7b995b5f9a133e2
Manas P <manas4518pachakkil@gmail.com>
2021-11-17 01:27:58 +0530

    Sidebar: Logo Section

---
+++ assets/pages/admin/admin_add_wm.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin | Add Ward Memeber</title>
+    <link rel="shortcut icon" href="../../images/fav.svg" type="image/x-icon">
+    <link rel="stylesheet" href="../../styles/admin_sidebar.css">
+    <link rel="stylesheet" href="../../styles/admin_add_wm.css">
+</head>
+<body>
+    <section class="main">
+        <div class="sidebar">
+            <div class="brand">
+                <img src="../../images/logo.svg" alt="logo" srcset="">
+                <div class="user">Admin</div>
+            </div>
+            <div class="division"></div>
+        </div>
+        <div class="container"></div>
+    </section>
+</body>
+</html>
