ef6377f7382d7e197fa2f272ab32f9c3f3e250a6
Manas P <manas4518pachakkil@gmail.com>
2022-02-04 05:25:53 +0530

    House member profile General informaion UI added

--- assets/pages/house_member/house_member_profile.php
+++ assets/pages/house_member/house_member_profile.php
@@ -58,7 +58,7 @@ else
                     <!-- menu -->
                     <div class="menu">
                         <div class="links">
-                            <a href="#" class="active">General informations</a>
+                            <a href="#general" class="active">General informations</a>
                             <a href="#" >Identity proofs</a>
                             <a href="#" >Educational background</a>
                             <a href="#" >Professional background</a>
@@ -67,7 +67,68 @@ else
                 </div>
                 
                 <div class="right">
-                    <h1 style="padding-bottom: 50px;">details</h1>
+
+                    <!-- General Indormation -->
+                    <section class="general" id="general">
+                        <div class="header">
+                            <div class="heading">
+                                General informations
+                            </div>
+                            <div class="edit">
+                                <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M10.0833 3.77783H3.66659C3.18036 3.77783 2.71404 3.97099 2.37022 4.3148C2.02641 4.65862 1.83325 5.12494 1.83325 5.61117V18.4445C1.83325 18.9307 2.02641 19.397 2.37022 19.7409C2.71404 20.0847 3.18036 20.2778 3.66659 20.2778H16.4999C16.9861 20.2778 17.4525 20.0847 17.7963 19.7409C18.1401 19.397 18.3333 18.9307 18.3333 18.4445V12.0278" stroke="#1E1E1E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                    <path d="M16.9583 2.40286C17.3229 2.03818 17.8175 1.83331 18.3333 1.83331C18.849 1.83331 19.3436 2.03818 19.7083 2.40286C20.0729 2.76753 20.2778 3.26213 20.2778 3.77786C20.2778 4.29358 20.0729 4.78818 19.7083 5.15286L10.9999 13.8612L7.33325 14.7779L8.24992 11.1112L16.9583 2.40286Z" stroke="#1E1E1E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                </svg>
+                            </div>
+                        </div>
+                        <div class="information">
+                            <div class="photo">
+                                <img src="../../images/uploads/photos/1637689913.png" alt="">
+                            </div>
+                            <div class="content">
+                                <div class="title">
+                                    Name:
+                                </div>
+                                <div class="info">
+                                    Wade Warren
+                                </div>
+                            </div>
+                            <div class="content">
+                                <div class="title">
+                                    Date of birth:
+                                </div>
+                                <div class="info">
+                                    05-10-1987
+                                </div>
+                            </div>
+                            <div class="content">
+                                <div class="title">
+                                    Blood group:
+                                </div>
+                                <div class="info">
+                                    O+
+                                </div>
+                            </div>
+                            <div class="content">
+                                <div class="title">
+                                    Email id:
+                                </div>
+                                <div class="info">
+                                    wadewarren12@gmail.com
+                                </div>
+                            </div>
+                            <div class="content">
+                                <div class="title">
+                                    Phone number:
+                                </div>
+                                <div class="info">
+                                    9878562354
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+
+                    
                 </div>
             </div>
         </section>
