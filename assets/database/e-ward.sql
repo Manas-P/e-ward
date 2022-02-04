f8e5942272a734521c4ae7c1afb517301b8c0a39
Manas P <manas4518pachakkil@gmail.com>
2022-02-04 15:43:47 +0530

    Database backup updated

--- assets/database/e-ward.sql
+++ assets/database/e-ward.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 03, 2022 at 04:19 PM
+-- Generation Time: Feb 04, 2022 at 11:12 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 7.4.25
 
@@ -48,7 +48,6 @@ INSERT INTO `tbl_admin` (`adid`, `username`, `password`) VALUES
 
 CREATE TABLE `tbl_house` (
   `hid` int(10) NOT NULL,
-  `rid` int(10) NOT NULL,
   `house_name` varchar(100) NOT NULL,
   `house_no` int(10) NOT NULL,
   `ward_no` int(10) NOT NULL,
@@ -62,14 +61,15 @@ CREATE TABLE `tbl_house` (
 -- Dumping data for table `tbl_house`
 --
 
-INSERT INTO `tbl_house` (`hid`, `rid`, `house_name`, `house_no`, `ward_no`, `locality`, `post_office`, `ration_no`, `category`) VALUES
-(1, 18, 'qweew', 121, 2, 'qweqw', 'qwqeweq', 4879856854, 'APL'),
-(2, 20, 'Pachakkil House', 145, 2, 'Kottooli', 'Kuthiravattom P.O.', 9568784568, 'APL'),
-(3, 27, 'Paramthottu', 154, 2, 'Pala', 'Chengalam', 1256325879, 'APL'),
-(4, 21, 'qwer', 124, 2, 'hjhyg', 'ikjjhy', 9865874585, 'APL'),
-(6, 31, 'qwert', 15, 2, 'fcrdrdcrd', 'wsxedxed', 8569857458, 'APL'),
-(7, 39, 'abcd villa', 5, 2, 'abcdefgh', 'abcdefgh', 4565434567, 'APL'),
-(15, 42, 'new house', 34, 2, 'new locality', 'new post office', 6548784521, 'APL');
+INSERT INTO `tbl_house` (`hid`, `house_name`, `house_no`, `ward_no`, `locality`, `post_office`, `ration_no`, `category`) VALUES
+(1, 'qweew', 121, 2, 'qweqw', 'qwqeweq', 4879856854, 'APL'),
+(2, 'Pachakkil House', 145, 2, 'Kottooli', 'Kuthiravattom P.O.', 9568784568, 'APL'),
+(3, 'Paramthottu', 154, 2, 'Pala', 'Chengalam', 1256325879, 'APL'),
+(4, 'qwer', 124, 2, 'hjhyg', 'ikjjhy', 9865874585, 'APL'),
+(6, 'qwert', 15, 2, 'fcrdrdcrd', 'wsxedxed', 8569857458, 'APL'),
+(7, 'abcd villa', 5, 2, 'abcdefgh', 'abcdefgh', 4565434567, 'APL'),
+(17, 'New housee', 34, 2, 'New locality', 'New post office', 5685458745, 'APL'),
+(18, 'new reg ho', 18, 2, 'new', 'new', 8459658745, 'APL');
 
 -- --------------------------------------------------------
 
@@ -86,22 +86,24 @@ CREATE TABLE `tbl_house_member` (
   `phno` bigint(13) NOT NULL,
   `blood_grp` varchar(5) NOT NULL DEFAULT 'NA',
   `dob` date NOT NULL,
-  `photo` varchar(100) NOT NULL DEFAULT '../images/user-profile-placeholder.png'
+  `photo` varchar(100) NOT NULL DEFAULT '../images/user-profile-placeholder.png',
+  `userid` int(50) NOT NULL DEFAULT 0,
+  `password` varchar(50) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tbl_house_member`
 --
 
-INSERT INTO `tbl_house_member` (`hm_id`, `ward_no`, `house_no`, `fname`, `email`, `phno`, `blood_grp`, `dob`, `photo`) VALUES
-(1, 2, 145, 'Annette Black', 'Not entered', 9658784526, 'O+', '1998-06-23', '../images/uploads/photos/1638009888.png'),
-(2, 2, 145, 'James Black', 'jamesblack@gmail.com', 9856587452, 'O+', '1995-03-15', '../images/uploads/photos/1638009946.png'),
-(3, 2, 154, 'Frank', 'fank@gmail.com', 9854569844, 'B-', '1998-09-17', '../images/uploads/photos/1638860482.jpg'),
-(4, 2, 154, 'Celia', 'celia@gmail.com', 9856587459, 'B-', '2001-10-01', '../images/uploads/photos/1638860567.png'),
-(5, 2, 145, 'Manas P', 'manas4518pachakkil@gmail.com', 9856587855, 'O+', '1998-10-28', '../images/uploads/photos/1639161827.png'),
-(9, 2, 34, 'new test', 'Not entered', 9858356478, 'NA', '0000-00-00', '../images/user-profile-placeholder.png'),
-(10, 2, 34, 'new user', 'newuser@gmail.com', 9854587452, 'B+', '2018-01-17', '../images/uploads/photos/1643901418.jpg'),
-(11, 2, 34, 'new new', 'new@gmail.com', 9884545129, 'O+', '1998-10-28', '../images/uploads/photos/1643901465.png');
+INSERT INTO `tbl_house_member` (`hm_id`, `ward_no`, `house_no`, `fname`, `email`, `phno`, `blood_grp`, `dob`, `photo`, `userid`, `password`) VALUES
+(1, 2, 145, 'Annette Black', 'Not entered', 9658784526, 'O+', '1998-06-23', '../images/uploads/photos/1638009888.png', 0, ''),
+(2, 2, 145, 'James Black', 'jamesblack@gmail.com', 9856587452, 'O+', '1995-03-15', '../images/uploads/photos/1638009946.png', 0, ''),
+(3, 2, 154, 'Frank', 'fank@gmail.com', 9854569844, 'B-', '1998-09-17', '../images/uploads/photos/1638860482.jpg', 0, ''),
+(4, 2, 154, 'Celia', 'celia@gmail.com', 9856587459, 'B-', '2001-10-01', '../images/uploads/photos/1638860567.png', 0, ''),
+(5, 2, 145, 'Manas P', 'manas4518pachakkil@gmail.com', 9856587855, 'O+', '1998-10-28', '../images/uploads/photos/1639161827.png', 0, ''),
+(15, 2, 34, 'new test', 'Not entered', 9858356478, 'NA', '0000-00-00', '../images/user-profile-placeholder.png', 2340, '123'),
+(16, 2, 18, 'new reg', 'Not entered', 5458965879, 'NA', '0000-00-00', '../images/user-profile-placeholder.png', 2180, '123'),
+(17, 2, 18, 'new regg', 'new@gmail.com', 9856587459, 'O+', '2019-07-10', '../images/uploads/photos/1643969508.png', 0, '0');
 
 -- --------------------------------------------------------
 
@@ -116,8 +118,6 @@ CREATE TABLE `tbl_registration` (
   `phno` bigint(15) NOT NULL,
   `wardno` bigint(10) NOT NULL,
   `houseno` bigint(10) NOT NULL,
-  `userid` bigint(10) DEFAULT NULL,
-  `password` varchar(60) NOT NULL DEFAULT '0',
   `taxreport` varchar(200) NOT NULL,
   `status` int(10) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -126,41 +126,40 @@ CREATE TABLE `tbl_registration` (
 -- Dumping data for table `tbl_registration`
 --
 
-INSERT INTO `tbl_registration` (`rid`, `fname`, `email`, `phno`, `wardno`, `houseno`, `userid`, `password`, `taxreport`, `status`) VALUES
-(1, 'Manas', 'manas@gmail.com', 9446184518, 15, 125, NULL, 'NuYivsek', '2458696351', 1),
-(2, 'mudhiu', 'manas@gmail.com', 4512365987, 25, 214, NULL, '0', '5369874512', 0),
-(3, 'asd', 'asd@gmail.com', 4125632569, 15, 125, NULL, '0', '5236598744', 0),
-(4, 'Man', 'man@gmail.com', 1234567898, 12, 145, NULL, '0', '1254589632', 0),
-(5, 'qwe', 'qwe@gmail.com', 4125632587, 45, 236, NULL, '0', '1254587452', 0),
-(6, 'telbincherian', 'ctelbin@gmail.com', 9605020907, 12, 124, NULL, '0', '7894561211', 0),
-(7, 'qw', 'qw@gmail.com', 1254125632, 14, 137, NULL, '0', '1256587856', 0),
-(8, 'qw', 'qw@gmail.com', 45125632547, 41, 47, NULL, '0', '4589632321', 0),
-(9, 'Appy Kurian', 'kuriantom@mca.ajce.in', 9548751256, 14, 123, 14123, '123', '1254896325', 1),
-(10, 'Amal Joy', 'amaljoy@mca.ajce.in', 8569878542, 14, 21, NULL, 'i3f69EAm', '1254589658', 1),
-(11, 'Akshai Biju', 'akshaibiju@mca.ajce.in', 4589652365, 17, 11, NULL, 'uj42Ayol', '1214526987', 1),
-(12, 'Kunni Bhai', 'telbincherian@mca.ajce.in', 7845698541, 12, 965, NULL, 'BmzxyDGj', '1458978745', 1),
-(13, 'Ebin Johnson', 'ebinjohnson@mca.ajce', 8545896587, 54, 456, NULL, 'GvjBPt3h', '9458965412', 1),
-(14, 'Anali Jose', 'josekemerson@mca.ajce.in', 9865487854, 14, 23, NULL, '5IE8TctU', '1254897785', 1),
-(15, 'Kozhi Sanju', 'sanjuabrahambinu@mca.ajce.in', 9548785468, 14, 234, NULL, '77gsp0th', '1254896587', 1),
-(16, 'Katta', 'akshaibiju3@gmail.com', 58965236587, 3, 14, NULL, '4PjLplOg', '1254895632', 1),
-(17, 'Anumol', 'anualexander@mca.ajce.in', 9565874587, 14, 128, NULL, 'Vtypy03e', '2548965236', 1),
-(18, 'aa', 'aa@gmail.com', 9854887541, 2, 121, NULL, '123', '4879856854', 1),
-(19, 'Akshai', 'akshaibiju@mca.ajce.in', 9856587458, 8, 12, NULL, 'MwhagcFF', '8754521254', 1),
-(20, 'Manas P', 'manas4513pachakkil@gmail.com', 9446184518, 2, 145, 2145, '123', '9568784568', 1),
-(21, 'Telbin Cherian', 'telbincherian@gmail.com', 9565878451, 2, 124, NULL, '123', '9865874585', 1),
-(22, 'Kurian Tom', 'kuriantom2017@gmail.com', 9856587458, 2, 12, NULL, '6D0Ab93X', '6587845213', 1),
-(25, 'Maamachan', 'albinpthomas@mca.ajce.in', 9856547854, 2, 158, NULL, '0', '6589856587', 0),
-(27, 'Frank Thomas', 'frankmathewsthomas@mca.ajce.in', 9856548785, 2, 154, NULL, '123', '1256325879', 1),
-(28, 'laali', 'laali@ed.fvv', 8785698745, 2, 147, NULL, '0', '1523658795', 0),
-(29, 'Laali Bhaai', 'laa@gmz.ff', 5485478965, 2, 14, NULL, '0', '1523659875', 0),
-(31, 'aaa', 'aaa@gmail.com', 9547586965, 2, 15, NULL, '1w4xL4O6', '8569857458', 1),
-(35, 'test', 'test@gmail.com', 5478965256, 2, 143, 2143, 'FZEra6FT', '1258963254', 1),
-(36, 'test', 'test@gmail.com', 5674567543, 2, 123, 2123, '0', '../documents/taxreport1643745313.txt', 0),
-(37, 'test', 'test@gmail.com', 3456765434, 2, 125, 2125, '0', '../documents/taxreport/1643745381.txt', 0),
-(38, 'test', 'test@gmail.com', 5456765679, 2, 56, 256, 'QydKPtOS', '../documents/taxreport/1643748995.pdf', 1),
-(39, 'test', 'test@gmail.com', 5467543345, 2, 5, 25, '123', '../documents/taxreport/1643751741.pdf', 1),
-(40, 'Manas test', 'manasp@mca.ajce.in', 7678976780, 2, 4, 24, 'aSpApfN6', '../documents/taxreport/1643752792.pdf', 1),
-(42, 'new test', 'newtest@gmail.com', 9858356478, 2, 34, 2340, '123', '../documents/taxreport/1643837559.pdf', 1);
+INSERT INTO `tbl_registration` (`rid`, `fname`, `email`, `phno`, `wardno`, `houseno`, `taxreport`, `status`) VALUES
+(1, 'Manas', 'manas@gmail.com', 9446184518, 15, 125, '2458696351', 1),
+(2, 'mudhiu', 'manas@gmail.com', 4512365987, 25, 214, '5369874512', 0),
+(3, 'asd', 'asd@gmail.com', 4125632569, 15, 125, '5236598744', 0),
+(4, 'Man', 'man@gmail.com', 1234567898, 12, 145, '1254589632', 0),
+(5, 'qwe', 'qwe@gmail.com', 4125632587, 45, 236, '1254587452', 0),
+(6, 'telbincherian', 'ctelbin@gmail.com', 9605020907, 12, 124, '7894561211', 0),
+(7, 'qw', 'qw@gmail.com', 1254125632, 14, 137, '1256587856', 0),
+(8, 'qw', 'qw@gmail.com', 45125632547, 41, 47, '4589632321', 0),
+(9, 'Appy Kurian', 'kuriantom@mca.ajce.in', 9548751256, 14, 123, '1254896325', 1),
+(10, 'Amal Joy', 'amaljoy@mca.ajce.in', 8569878542, 14, 21, '1254589658', 1),
+(11, 'Akshai Biju', 'akshaibiju@mca.ajce.in', 4589652365, 17, 11, '1214526987', 1),
+(12, 'Kunni Bhai', 'telbincherian@mca.ajce.in', 7845698541, 12, 965, '1458978745', 1),
+(13, 'Ebin Johnson', 'ebinjohnson@mca.ajce', 8545896587, 54, 456, '9458965412', 1),
+(14, 'Anali Jose', 'josekemerson@mca.ajce.in', 9865487854, 14, 23, '1254897785', 1),
+(15, 'Kozhi Sanju', 'sanjuabrahambinu@mca.ajce.in', 9548785468, 14, 234, '1254896587', 1),
+(16, 'Katta', 'akshaibiju3@gmail.com', 58965236587, 3, 14, '1254895632', 1),
+(17, 'Anumol', 'anualexander@mca.ajce.in', 9565874587, 14, 128, '2548965236', 1),
+(18, 'aa', 'aa@gmail.com', 9854887541, 2, 121, '4879856854', 1),
+(19, 'Akshai', 'akshaibiju@mca.ajce.in', 9856587458, 8, 12, '8754521254', 1),
+(20, 'Manas P', 'manas4513pachakkil@gmail.com', 9446184518, 2, 145, '9568784568', 1),
+(21, 'Telbin Cherian', 'telbincherian@gmail.com', 9565878451, 2, 124, '9865874585', 1),
+(22, 'Kurian Tom', 'kuriantom2017@gmail.com', 9856587458, 2, 12, '6587845213', 1),
+(25, 'Maamachan', 'albinpthomas@mca.ajce.in', 9856547854, 2, 158, '6589856587', 0),
+(27, 'Frank Thomas', 'frankmathewsthomas@mca.ajce.in', 9856548785, 2, 154, '1256325879', 1),
+(28, 'laali', 'laali@ed.fvv', 8785698745, 2, 147, '1523658795', 0),
+(29, 'Laali Bhaai', 'laa@gmz.ff', 5485478965, 2, 14, '1523659875', 0),
+(31, 'aaa', 'aaa@gmail.com', 9547586965, 2, 15, '8569857458', 1),
+(35, 'test', 'test@gmail.com', 5478965256, 2, 143, '1258963254', 1),
+(38, 'test', 'test@gmail.com', 5456765679, 2, 56, '../documents/taxreport/1643748995.pdf', 1),
+(39, 'test', 'test@gmail.com', 5467543345, 2, 5, '../documents/taxreport/1643751741.pdf', 1),
+(40, 'Manas test', 'manasp@mca.ajce.in', 7678976780, 2, 4, '../documents/taxreport/1643752792.pdf', 1),
+(42, 'new test', 'newtest@gmail.com', 9858356478, 2, 34, '../documents/taxreport/1643837559.pdf', 1),
+(43, 'new reg', 'newreg@gmail.com', 5458965879, 2, 18, '../documents/taxreport/1643969173.pdf', 1);
 
 -- --------------------------------------------------------
 
@@ -245,19 +244,19 @@ ALTER TABLE `tbl_admin`
 -- AUTO_INCREMENT for table `tbl_house`
 --
 ALTER TABLE `tbl_house`
-  MODIFY `hid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `hid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `tbl_house_member`
 --
 ALTER TABLE `tbl_house_member`
-  MODIFY `hm_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `hm_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `tbl_registration`
 --
 ALTER TABLE `tbl_registration`
-  MODIFY `rid` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+  MODIFY `rid` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT for table `tbl_ward_member`
