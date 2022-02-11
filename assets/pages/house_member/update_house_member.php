6a800c5b4c076fd2c47801ad1a751d9297bdf806
Manas P <manas4518pachakkil@gmail.com>
2022-02-12 01:34:19 +0530

    hm edit profile - General info ui added

--- assets/pages/house_member/update_house_member.php
+++ assets/pages/house_member/update_house_member.php
@@ -39,7 +39,7 @@ else
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>E Ward</title>
         <link rel="shortcut icon" href="../../images/fav.svg" type="image/x-icon">
-        <link rel="stylesheet" href="../../styles/hm_profile.css">
+        <link rel="stylesheet" href="../../styles/hm_profile_update.css">
     </head>
     <body>
         <section class="main">
@@ -59,10 +59,16 @@ else
                         <svg class="str" width="8" height="10" viewBox="0 0 8 10" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M2.2002 8.59999L5.8002 4.99999L2.2002 1.39999" stroke="#1E1E1E" stroke-linecap="round" stroke-linejoin="round" />
                         </svg>
-                        <a href="" class="now">
+                        <a href="./house_member_profile.php?id=<?php echo $hm_id; ?>" class="previous">
                             <?php
                                 echo $name;
                             ?>
+                        </a>
+                        <svg class="str" width="8" height="10" viewBox="0 0 8 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M2.2002 8.59999L5.8002 4.99999L2.2002 1.39999" stroke="#1E1E1E" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
+                        <a href="" class="now">
+                            Edit profile
                         </a>
                     </div>
                     <!-- menu -->
@@ -74,6 +80,9 @@ else
                             <a href="#" >Professional background</a>
                         </div>
                     </div>
+                    <div class="buttn">
+                        <input type="button" value="Update changes" class="button">
+                    </div>
                 </div>
                 
                 <div class="right">
@@ -84,63 +93,58 @@ else
                             <div class="heading">
                                 General informations
                             </div>
-                            <?php
-                                if($chk==0 || $chk==$chk2){
-                            ?>
-                                    <div class="edit">
-                                        <a href="./update_house_member.php?id=<?php echo $hm_id ?>">
-                                            <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M10.0833 3.77783H3.66659C3.18036 3.77783 2.71404 3.97099 2.37022 4.3148C2.02641 4.65862 1.83325 5.12494 1.83325 5.61117V18.4445C1.83325 18.9307 2.02641 19.397 2.37022 19.7409C2.71404 20.0847 3.18036 20.2778 3.66659 20.2778H16.4999C16.9861 20.2778 17.4525 20.0847 17.7963 19.7409C18.1401 19.397 18.3333 18.9307 18.3333 18.4445V12.0278" stroke="#1E1E1E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                                                <path d="M16.9583 2.40286C17.3229 2.03818 17.8175 1.83331 18.3333 1.83331C18.849 1.83331 19.3436 2.03818 19.7083 2.40286C20.0729 2.76753 20.2778 3.26213 20.2778 3.77786C20.2778 4.29358 20.0729 4.78818 19.7083 5.15286L10.9999 13.8612L7.33325 14.7779L8.24992 11.1112L16.9583 2.40286Z" stroke="#1E1E1E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                                            </svg>
-                                        </a>
-                                    </div>
-                            <?php
-                                }
-                            ?>
                         </div>
                         <div class="information">
-                            <div class="photo">
-                                <img src="../<?php echo $photo; ?>" alt="">
-                            </div>
-                            <div class="content">
-                                <div class="title">
-                                    Name:
+                            <div class="inputs">
+                                <div class="input h-photo">
+                                    <div class="label">
+                                        Upload photo
+                                    </div>
+                                    <input type="file" name="hphoto" id="h-photo" accept="image/png,image/jpeg">
+                                    <div class="error error-hidden">
+                                    </div>
                                 </div>
-                                <div class="info">
-                                    <?php echo $name; ?>
+                                <div class="input h-fullname">
+                                    <div class="label">
+                                        Full name
+                                    </div>
+                                    <input type="text" name="hfname" id="h-full-name" placeholder="John Doe" autocomplete="off">
+                                    <div class="error error-hidden">
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="content">
-                                <div class="title">
-                                    Date of birth:
+                                <div class="half-input">
+                                    <div class="input h-date">
+                                        <div class="label">
+                                            Date of birth
+                                        </div>
+                                        <input type="date" name="hdob" id="h-date" autocomplete="off">
+                                        <div class="error error-hidden">
+                                        </div>
+                                    </div>
+                                    <div class="input h-blood">
+                                        <div class="label">
+                                            Blood group
+                                        </div>
+                                        <input type="text" name="hblood" id="h-blood" placeholder="A+" autocomplete="off">
+                                        <div class="error error-hidden">
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class="info">
-                                    <?php echo $dob; ?>
+                                <div class="input h-email">
+                                    <div class="label">
+                                        Email ID
+                                    </div>
+                                    <input type="text" name="hemail" id="h-email-id" placeholder="example@gmail.com" autocomplete="off">
+                                    <div class="error error-hidden">
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="content">
-                                <div class="title">
-                                    Blood group:
-                                </div>
-                                <div class="info">
-                                    <?php echo $blood; ?>
-                                </div>
-                            </div>
-                            <div class="content">
-                                <div class="title">
-                                    Email id:
-                                </div>
-                                <div class="info">
-                                    <?php echo $email; ?>
-                                </div>
-                            </div>
-                            <div class="content">
-                                <div class="title">
-                                    Phone number:
-                                </div>
-                                <div class="info">
-                                    <?php echo $phno; ?>
+                                <div class="input h-phno">
+                                    <div class="label">
+                                        Phone number
+                                    </div>
+                                    <input type="text" name="hphno" id="h-phn-number" placeholder="9568547512" autocomplete="off">
+                                    <div class="error error-hidden">
+                                    </div>
                                 </div>
                             </div>
                         </div>
