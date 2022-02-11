038344f1f7622cc4212de709100826d0b7ec073f
Manas P <manas4518pachakkil@gmail.com>
2022-02-11 20:48:45 +0530

    House member - First login sidebar fix

--- assets/include/house_member/sidebar_hm_update.php
+++ assets/include/house_member/sidebar_hm_update.php
@@ -22,7 +22,7 @@
                             </svg>
                             <div class="text active">Update House Info</div>
                         </a>
-                        <a href="../../pages/house_member/add_house_members.php" class="menu">
+                        <a href="../../pages/house_member/dashboard.php" class="menu">
                             <svg width="24" height="22" viewBox="0 0 24 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path class="str" d="M15.6667 19.25V17.4167C15.6667 16.4442 15.2804 15.5116 14.5927 14.8239C13.9051 14.1363 12.9725 13.75 12 13.75H5.58335C4.61089 13.75 3.67826 14.1363 2.99063 14.8239C2.303 15.5116 1.91669 16.4442 1.91669 17.4167V19.25" stroke="#1E1E1E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                                 <path class="str" d="M8.79167 10.0833C10.8167 10.0833 12.4583 8.44171 12.4583 6.41667C12.4583 4.39162 10.8167 2.75 8.79167 2.75C6.76662 2.75 5.125 4.39162 5.125 6.41667C5.125 8.44171 6.76662 10.0833 8.79167 10.0833Z" stroke="#1E1E1E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
