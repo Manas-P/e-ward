dcb709ca610338217f029bdf90f67715f606010d
Manas P <manas4518pachakkil@gmail.com>
2022-02-19 23:09:11 +0530

    hm profile - Added professional background section

--- assets/php/approve.php
+++ assets/php/approve.php
@@ -30,7 +30,11 @@
 
     if(mail($toMail,$subject,$body,$headers)){
         //Update User Status and Password (Multi-query)
-        $updateQuery="UPDATE `tbl_registration` SET `status`=1 WHERE `rid`='$id' ; INSERT INTO `tbl_house_member`(`ward_no`, `house_no`, `fname`, `phno`, `userid`, `password`) VALUES ('$wardno','$houseNo','$name','$phno','$userid','$generatedPassword') ; INSERT INTO `tbl_id_proof`(`userid`) VALUES ('$userid')";
+        $updateQuery="UPDATE `tbl_registration` SET `status`=1 WHERE `rid`='$id' ; 
+        INSERT INTO `tbl_house_member`(`ward_no`, `house_no`, `fname`, `phno`, `userid`, `password`) VALUES ('$wardno','$houseNo','$name','$phno','$userid','$generatedPassword') ; 
+        INSERT INTO `tbl_id_proof`(`userid`) VALUES ('$userid') ; 
+        INSERT INTO `tbl_edu_bg`(`userid`) VALUES ('$userid') ;
+        INSERT INTO `tbl_pro_bg`(`userid`) VALUES ('$userid')";
         $updateResult=mysqli_multi_query($conn,$updateQuery);
         
         $_SESSION['loginMessage'] = "House approved";
