86b478fced4667dc14d34bcfb9589c6babe0f92c
Manas P <manas4518pachakkil@gmail.com>
2022-03-24 22:46:03 +0530

    Restructure - Pdf issue solved

--- application/model/viewPdf.php
+++ application/model/viewPdf.php
@@ -3,6 +3,12 @@
     session_start();
     //Fetch pdf data
     $pdf=$_GET['pdf'];
+
+    //Identify folder path
+    $find="taxreport";
+    if(strpos($pdf, $find)==false){
+        $pdf=substr($pdf, 3);
+    }
     
     // Header content type
     header("Content-type: application/pdf");
@@ -12,5 +18,3 @@
     // Send the file to the browser.
     readfile($pdf);
 ?>
-<!-- ../../public/assets/documents/
-../../../public/assets/documents/ -->
