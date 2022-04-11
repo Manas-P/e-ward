b70a8d602372f0ba85f3db6fe03bf2b764c96e57
Manas P <manas4518pachakkil@gmail.com>
2022-04-11 17:26:32 +0530

    Ward member - Change password

--- application/view/layout/ward_member/sidebar_change_password.php
+++ application/view/layout/ward_member/sidebar_change_password.php
@@ -1,4 +1,4 @@
-            <div class="sidebar" style="position: fixed;">
+<div class="sidebar" style="position: fixed;">
                 <div class="brand">
                     <img src="../../../../public/assets/images/logo.svg" alt="logo" srcset="">
                     <div class="user">Ward: <?php echo $wardno ?></div>
@@ -15,12 +15,12 @@
                             </svg>
                             <div class="text">Dashboard</div>
                         </a>
-                        <a href="../../pages/ward_member/houses.php" class="menu active">
+                        <a href="../../pages/ward_member/houses.php" class="menu">
                             <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path class="str active" d="M8.25 20.1667H4.58333C4.0971 20.1667 3.63079 19.9736 3.28697 19.6297C2.94315 19.2859 2.75 18.8196 2.75 18.3334V8.25004L11 1.83337L19.25 8.25004V18.3334C19.25 18.8196 19.0568 19.2859 18.713 19.6297C18.3692 19.9736 17.9029 20.1667 17.4167 20.1667H13.75" stroke="#1E1E1E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                                <path class="str active" d="M8.25 20.1667V11H13.75V20.1667" stroke="#1E1E1E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path class="str" d="M8.25 20.1667H4.58333C4.0971 20.1667 3.63079 19.9736 3.28697 19.6297C2.94315 19.2859 2.75 18.8196 2.75 18.3334V8.25004L11 1.83337L19.25 8.25004V18.3334C19.25 18.8196 19.0568 19.2859 18.713 19.6297C18.3692 19.9736 17.9029 20.1667 17.4167 20.1667H13.75" stroke="#1E1E1E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path class="str" d="M8.25 20.1667V11H13.75V20.1667" stroke="#1E1E1E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                             </svg>
-                            <div class="text active">Houses</div>
+                            <div class="text">Houses</div>
                         </a>
                         <a href="../../pages/ward_member/add_office_staff.php" class="menu">
                             <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -38,12 +38,12 @@
                             </svg>                                
                             <div class="text">Committees</div>
                         </a>
-                        <a href="../../pages/ward_member/change_password.php" class="menu">
+                        <a href="../../pages/ward_member/change_password.php" class="menu active">
                             <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path class="str" d="M17.4167 10.0835H4.58339C3.57087 10.0835 2.75006 10.9043 2.75006 11.9168V18.3335C2.75006 19.346 3.57087 20.1668 4.58339 20.1668H17.4167C18.4292 20.1668 19.2501 19.346 19.2501 18.3335V11.9168C19.2501 10.9043 18.4292 10.0835 17.4167 10.0835Z" stroke="#1E1E1E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                                <path class="str" d="M6.41681 10.0835V6.41683C6.41681 5.20125 6.89969 4.03547 7.75924 3.17592C8.61878 2.31638 9.78457 1.8335 11.0001 1.8335C12.2157 1.8335 13.3815 2.31638 14.241 3.17592C15.1006 4.03547 15.5835 5.20125 15.5835 6.41683V10.0835" stroke="#1E1E1E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path class="str active" d="M17.4167 10.0835H4.58339C3.57087 10.0835 2.75006 10.9043 2.75006 11.9168V18.3335C2.75006 19.346 3.57087 20.1668 4.58339 20.1668H17.4167C18.4292 20.1668 19.2501 19.346 19.2501 18.3335V11.9168C19.2501 10.9043 18.4292 10.0835 17.4167 10.0835Z" stroke="#1E1E1E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path class="str active" d="M6.41681 10.0835V6.41683C6.41681 5.20125 6.89969 4.03547 7.75924 3.17592C8.61878 2.31638 9.78457 1.8335 11.0001 1.8335C12.2157 1.8335 13.3815 2.31638 14.241 3.17592C15.1006 4.03547 15.5835 5.20125 15.5835 6.41683V10.0835" stroke="#1E1E1E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                             </svg>
-                            <div class="text">Change password</div>
+                            <div class="text active">Change password</div>
                         </a>
                     </div>
                     <div class="logout">
--- application/view/layout/ward_member/sidebar_office_staff.php
+++ application/view/layout/ward_member/sidebar_office_staff.php
@@ -38,6 +38,13 @@
                             </svg>                                
                             <div class="text">Committees</div>
                         </a>
+                        <a href="../../pages/ward_member/change_password.php" class="menu">
+                            <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path class="str" d="M17.4167 10.0835H4.58339C3.57087 10.0835 2.75006 10.9043 2.75006 11.9168V18.3335C2.75006 19.346 3.57087 20.1668 4.58339 20.1668H17.4167C18.4292 20.1668 19.2501 19.346 19.2501 18.3335V11.9168C19.2501 10.9043 18.4292 10.0835 17.4167 10.0835Z" stroke="#1E1E1E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path class="str" d="M6.41681 10.0835V6.41683C6.41681 5.20125 6.89969 4.03547 7.75924 3.17592C8.61878 2.31638 9.78457 1.8335 11.0001 1.8335C12.2157 1.8335 13.3815 2.31638 14.241 3.17592C15.1006 4.03547 15.5835 5.20125 15.5835 6.41683V10.0835" stroke="#1E1E1E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                            <div class="text">Change password</div>
+                        </a>
                     </div>
                     <div class="logout">
                         <a href="../../../controller/logout.php">
