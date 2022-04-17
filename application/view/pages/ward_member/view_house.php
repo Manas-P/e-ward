c07763e9f20c96d26ff256cbedae757e348fe52c
Manas P <manas4518pachakkil@gmail.com>
2022-04-17 17:09:23 +0530

    View house - Boilerplate added

--- application/view/pages/ward_member/view_house.php
+++ application/view/pages/ward_member/view_house.php
@@ -13,6 +13,95 @@
         $name=$_SESSION['fullname'];
 
         $houseno=$_GET['houseno'];
-        echo $houseno;
-    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="shortcut icon" href="../../../../public/assets/images/fav.svg" type="image/x-icon">
+        <link rel="stylesheet" href="../../../../public/assets/css/ward_member/view_house.css">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+        <title>Ward <?php echo $wardno; ?> || View house</title>
+    </head>
+    <body>
+        <section class="main">
+            <!-- ==========Sidebar============= -->
+            <?php
+            include '../../layout/ward_member/sidebar_houses.php';
+            ?>
+            <!-- ==========Sidebar End============= -->
+            <div class="container">
+                <div class="header">
+                    <div class="title">
+                        Manage houses
+                    </div>
+                    <div class="search">
+                        <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path class="str" d="M8.25 14.25C11.5637 14.25 14.25 11.5637 14.25 8.25C14.25 4.93629 11.5637 2.25 8.25 2.25C4.93629 2.25 2.25 4.93629 2.25 8.25C2.25 11.5637 4.93629 14.25 8.25 14.25Z" stroke="#B1B1B1" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path class="str" d="M15.7498 15.75L12.4873 12.4875" stroke="#B1B1B1" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>                        
+                        <input type="text" name="" placeholder="Search..." id="">
+                    </div>
+                </div>
+                <div class="bread-crumbs">
+                    <a href="./houses.php" class="previous">
+                        Houses
+                    </a>
+                    <svg class="str" width="8" height="10" viewBox="0 0 8 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M2.2002 8.59999L5.8002 4.99999L2.2002 1.39999" stroke="#1E1E1E" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                    <a href="" class="now">
+                        House no. <?php echo $houseno; ?>
+                    </a>
+                </div>
+                
+            </div>
+        </section>
+
+
+        <!-- Error Toast -->
+        <?php
+        if (isset($_SESSION['error'])) {
+            $msg=$_SESSION['error'];
+          echo " <div class='alertt alert-visible'> 
+                        <div class='econtent'>
+                            <img src='../../../../public/assets/images/warning.svg' alt='warning'>
+                            <div class='text'>
+                                $msg
+                            </div>
+                        </div>
+                        <img src='../../../../public/assets/images/close.svg' alt='close' class='alert-close'>
+                    </div>";
+          unset($_SESSION['error']);
+        }?>
+
+        <!-- Success toast -->
+        <?php
+            if (isset($_SESSION['success'])) {
+                $msg=$_SESSION['success'];
+                echo " <div class='alertt alert-visible' style='border-left: 10px solid #1BBD2B;'>
+                            <div class='econtent'>
+                                <img src='../../../../public/assets/images/check.svg' alt='success'>
+                                <div class='text'>
+                                    $msg
+                                </div>
+                            </div>
+                            <img src='../../../../public/assets/images/close.svg' alt='close' class='alert-close'>
+                        </div>";
+                unset($_SESSION['success']);
+        }?>
+
+        <!-- ==========Loading============= -->
+        <?php
+            include '../../includes/loading.php';
+        ?>
+        <!-- ==========Loading End============= -->
+        <script src="../../../../public/assets/js/toast.js"></script>
+    </body>
+</html>
+	<?php
+}
 ?>
