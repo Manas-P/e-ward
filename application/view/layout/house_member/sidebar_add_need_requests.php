76923591b735d4bcbd5c05cdaf63f24117c77aa6
Manas P <manas4518pachakkil@gmail.com>
2022-05-03 03:14:30 +0530

    Hm add need request ui added

--- application/view/layout/house_member/sidebar_add_need_requests.php
+++ application/view/layout/house_member/sidebar_add_need_requests.php
@@ -40,21 +40,21 @@
                 </svg>                                
                 <div class="text">Committees</div>
             </a>
-            <a href="../../pages/house_member/add_need_request.php" class="menu">
+            <a href="../../pages/house_member/add_need_request.php" class="menu active">
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path class="str" d="M16 4H18C18.5304 4 19.0391 4.21071 19.4142 4.58579C19.7893 4.96086 20 5.46957 20 6V20C20 20.5304 19.7893 21.0391 19.4142 21.4142C19.0391 21.7893 18.5304 22 18 22H6C5.46957 22 4.96086 21.7893 4.58579 21.4142C4.21071 21.0391 4 20.5304 4 20V6C4 5.46957 4.21071 4.96086 4.58579 4.58579C4.96086 4.21071 5.46957 4 6 4H8" stroke="#1E1E1E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                    <path class="str" d="M15 2H9C8.44772 2 8 2.44772 8 3V5C8 5.55228 8.44772 6 9 6H15C15.5523 6 16 5.55228 16 5V3C16 2.44772 15.5523 2 15 2Z" stroke="#1E1E1E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                    <path class="str" d="M12 10V16" stroke="#1E1E1E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                    <path class="str" d="M15 13H9" stroke="#1E1E1E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path class="str active" d="M16 4H18C18.5304 4 19.0391 4.21071 19.4142 4.58579C19.7893 4.96086 20 5.46957 20 6V20C20 20.5304 19.7893 21.0391 19.4142 21.4142C19.0391 21.7893 18.5304 22 18 22H6C5.46957 22 4.96086 21.7893 4.58579 21.4142C4.21071 21.0391 4 20.5304 4 20V6C4 5.46957 4.21071 4.96086 4.58579 4.58579C4.96086 4.21071 5.46957 4 6 4H8" stroke="#1E1E1E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path class="str active" d="M15 2H9C8.44772 2 8 2.44772 8 3V5C8 5.55228 8.44772 6 9 6H15C15.5523 6 16 5.55228 16 5V3C16 2.44772 15.5523 2 15 2Z" stroke="#1E1E1E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path class="str active" d="M12 10V16" stroke="#1E1E1E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path class="str active" d="M15 13H9" stroke="#1E1E1E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>
-                <div class="text">Add need requests</div>
+                <div class="text active">Add need requests</div>
             </a>
-            <a href="../../pages/house_member/change_password.php" class="menu active">
+            <a href="../../pages/house_member/change_password.php" class="menu">
                 <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path class="str active" d="M17.4167 10.0835H4.58339C3.57087 10.0835 2.75006 10.9043 2.75006 11.9168V18.3335C2.75006 19.346 3.57087 20.1668 4.58339 20.1668H17.4167C18.4292 20.1668 19.2501 19.346 19.2501 18.3335V11.9168C19.2501 10.9043 18.4292 10.0835 17.4167 10.0835Z" stroke="#1E1E1E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                    <path class="str active" d="M6.41681 10.0835V6.41683C6.41681 5.20125 6.89969 4.03547 7.75924 3.17592C8.61878 2.31638 9.78457 1.8335 11.0001 1.8335C12.2157 1.8335 13.3815 2.31638 14.241 3.17592C15.1006 4.03547 15.5835 5.20125 15.5835 6.41683V10.0835" stroke="#1E1E1E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path class="str" d="M17.4167 10.0835H4.58339C3.57087 10.0835 2.75006 10.9043 2.75006 11.9168V18.3335C2.75006 19.346 3.57087 20.1668 4.58339 20.1668H17.4167C18.4292 20.1668 19.2501 19.346 19.2501 18.3335V11.9168C19.2501 10.9043 18.4292 10.0835 17.4167 10.0835Z" stroke="#1E1E1E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path class="str" d="M6.41681 10.0835V6.41683C6.41681 5.20125 6.89969 4.03547 7.75924 3.17592C8.61878 2.31638 9.78457 1.8335 11.0001 1.8335C12.2157 1.8335 13.3815 2.31638 14.241 3.17592C15.1006 4.03547 15.5835 5.20125 15.5835 6.41683V10.0835" stroke="#1E1E1E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>
-                <div class="text active">Change password</div>
+                <div class="text">Change password</div>
             </a>
         </div>
         <div class="logout">
